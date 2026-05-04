9561e0101e29a659ee48b1b60342a4790d6efc48
methodmystic <pranavshinde9906@gmail.com>
2026-05-04 23:13:06 +0530

    fix issue

--- src/main/java/servlet/AddexpenseServlet.java
+++ src/main/java/servlet/AddexpenseServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
+import dao.ExpenseDao;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import model.Expense;
+import model.User;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
 
 public class AddexpenseServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -13,5 +19,20 @@ public class AddexpenseServlet {
         double amount = Double.parseDouble(request.getParameter("amount"));
         String category = request.getParameter("category");
         String dateStr = request.getParameter("date");
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        int userId = user.getId();
+
+        Expense expense = new Expense(0, userId, title, amount, category, Date);
+
+        try {
+            ExpenseDao expenseDao = new ExpenseDao();
+            expenseDao.addExpense(expense);
+            response.sendRedirect("dashboard");
+        } catch (SQLException | ClassNotFoundException | ParseException e) {
+            e.printStackTrace();
+            response.sendRedirect("add-expense.jsp?error=failed");
+        }
     }
 }
